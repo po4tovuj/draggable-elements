9e0d5eb82c054a096693c54ce44f1def83e61651
Sergey Miniaylo <po4tovuj@gmail.com>
2022-07-04 16:02:43 +0300

    moved navbar to header componen

---
+++ components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <header class="header">
+    <nav
+      class="navbar has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img src="~assets/buefy.png" alt="Buefy" height="28" />
+        </a>
+      </div>
+      <ul class="nav-link-list">
+        <li>
+          <NuxtLink
+            exact
+            to="/"
+            class="nav-link"
+            active-class="nav-link--active"
+          >
+            <b-icon icon="home" /> Home
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/albums"
+            class="nav-link"
+            active-class="nav-link--active"
+          >
+            <b-icon icon="folder-multiple-image" /> Albums
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <b-button class="header__create-btn" @click="createAlbum"
+      >Create Album</b-button
+    >
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' },
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  &__create-btn {
+    margin-left: auto;
+  }
+  .navbar {
+    padding: 0 20px;
+  }
+  .nav-link-list {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    .nav-link {
+      color: white;
+      font-size: 20px;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,6 @@
 <template>
   <div>
-    <header class="header">
-      <nav
-        class="navbar has-shadow is-primary"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div class="navbar-brand">
-          <a class="navbar-item" href="/">
-            <img src="~assets/buefy.png" alt="Buefy" height="28" />
-          </a>
-        </div>
-        <ul class="nav-link-list">
-          <li>
-            <NuxtLink
-              exact
-              to="/"
-              class="nav-link"
-              active-class="nav-link--active"
-            >
-              <b-icon icon="home" /> Home
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink
-              to="/albums"
-              class="nav-link"
-              active-class="nav-link--active"
-            >
-              <b-icon icon="folder-multiple-image" /> Albums
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Header />
     <section class="main-content columns">
       <div class="container column is-10">
         <Nuxt />
@@ -45,34 +12,9 @@
 <script>
 export default {
   name: 'DefaultLayout',
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' },
-        },
-      ],
-    }
-  },
 }
 </script>
 <style lang="scss">
-.header {
-  .navbar {
-    padding: 0 20px;
-  }
-  .nav-link-list {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    .nav-link {
-      color: white;
-      font-size: 20px;
-    }
-  }
-}
 .container {
   padding-top: 2em;
   padding-bottom: 2em;
