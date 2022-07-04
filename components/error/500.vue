c9cfbac438ac8c7680869d6cb40f5692c40bc352
Sergey Miniaylo <po4tovuj@gmail.com>
2022-07-04 16:01:33 +0300

    error pages created

---
+++ components/error/500.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="error-404">
+    <h1>Internal server error</h1>
+    <p>{{ error.message }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'error-404',
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
